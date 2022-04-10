0b747aaa98750c9776c6bcd4eb0c47c061fcfb95
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-04-11 00:27:27 +0300

    Repository Design Pattern icin ana repositoryler olusturuldu

---
+++ MiniETicaretAPI/Core/MiniETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,21 @@
+﻿using MiniETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        // IQueryable ile calistigimiz zaman verilere sorgular ile erismis oluruz.
+        // IEnumerable ile calisirsak verilen once inmemory'e cekilir ve oradan erisim saglamis oluruz.
+
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
+    }
+}
---
+++ MiniETicaretAPI/Infrastructure/MiniETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniETicaretAPI.Application.Repositories;
+using MiniETicaretAPI.Domain.Entities.Common;
+using MiniETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniETicaretAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly MiniETicaretAPIDbContext _context;
+
+        public ReadRepository(MiniETicaretAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll() => Table;
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method) => Table.Where(method);
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method) => await Table.FirstOrDefaultAsync(method);
+
+        public async Task<T> GetByIdAsync(string id) => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+    }
+}
