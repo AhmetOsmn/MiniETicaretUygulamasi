9cec0e2e4306cd4c0cc8d92f4339be07d332ad2b
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-04-30 15:52:46 +0300

    urun ekleme islemi tamamlandi

--- MiniETicaretClient/src/app/admin/components/products/products.component.ts
+++ MiniETicaretClient/src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,36 +15,5 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallSpinClockwiseFadeRotating);
-
-
-    // get 
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data))
-
-    //post 
-    // this.httpClientService.post({
-    //   controller: "products",
-    // }, {
-    //   name: "cetvel",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
-
-    //put 
-    // this.httpClientService.put({
-    //   controller: "products",
-    // }, {
-    //   id: "ee7f82a7-d02a-4eb8-b677-023022ac2642",
-    //   name: "Renkli kagit",
-    //   stock: 1500,
-    //   price: 5.5
-    // }).subscribe();
-
-    //delete 
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // }, "05e99db4-9754-49c3-879b-e01f759aa3d6").subscribe();
-
   }
 }
--- MiniETicaretClient/src/app/admin/components/products/products.module.ts
+++ MiniETicaretClient/src/app/admin/components/products/products.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
+
+import { ProductsComponent } from './products.component';
+import { CreateComponent } from './create/create.component';
+import { ListComponent } from './list/list.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    CreateComponent,
+    ListComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path: "", component: ProductsComponent}
-    ])
+    ]),
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+
   ]
 })
 export class ProductsModule { }
