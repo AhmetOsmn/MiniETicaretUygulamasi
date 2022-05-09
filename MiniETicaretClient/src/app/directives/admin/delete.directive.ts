e8348eddd51b63e47b523f2240829eddc9e4f0d0
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-09 23:55:36 +0300

    Delete Directive optimize edildi

--- MiniETicaretClient/src/app/directives/admin/delete.directive.ts
+++ MiniETicaretClient/src/app/directives/admin/delete.directive.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import {
   Directive,
   ElementRef,
@@ -14,6 +15,8 @@ import {
   DeleteDialogComponent,
   DeleteSate,
 } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 declare var $: any;
@@ -25,9 +28,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
+    private httpClientService: HttpClientService,
+    private alertifyService: AlertifyService,
     private spinner: NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
   ) {
     const img = _renderer.createElement('img');
     img.setAttribute('src', '../../../../../assets/delete.png');
@@ -38,6 +42,7 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string;
   @Output() callback: EventEmitter<any> = new EventEmitter();
 
   @HostListener('click')
@@ -45,18 +50,38 @@ export class DeleteDirective {
     this.openDialog(async () => {
       this.spinner.show(SpinnerType.BallAtom);
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate(
-        {
-          opacity: 0,
-          left: '+=50',
-          height: 'toggle',
-        },
-        700,
-        () => {
-          this.callback.emit();
-        }
-      );
+      this.httpClientService
+        .delete(
+          {
+            controller: this.controller,
+          },
+          this.id
+        )
+        .subscribe((data) => {
+          $(td.parentElement).animate(
+            {
+              opacity: 0,
+              left: '+=50',
+              height: 'toggle',
+            },
+            700,
+            () => {
+              this.callback.emit();
+              this.alertifyService.message("Ürün başarıyla silindi.", {
+                dismisssOthers: true,
+                messageType: MessageType.Success,
+                position: Position.TopRight
+              });
+            }
+          );
+        }, (errorResponse: HttpErrorResponse) => {
+          this.spinner.hide(SpinnerType.BallAtom)
+          this.alertifyService.message("Ürün silinemedi.", {
+            dismisssOthers: true,
+            messageType: MessageType.Error,
+            position: Position.TopRight
+          });
+        });
     });
   }
 
